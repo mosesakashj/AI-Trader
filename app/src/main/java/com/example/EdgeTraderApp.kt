079f21395d27ce904bf2026b4d8185f952172507
mosesakashj <mosesakash.j@gmail.com>
2026-08-22 15:07:28 +0530

    Improvmetns

--- app/src/main/java/com/example/EdgeTraderApp.kt
+++ app/src/main/java/com/example/EdgeTraderApp.kt
@@ -12,6 +12,7 @@ import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
